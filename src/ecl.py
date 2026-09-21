f4f2e800472fd916095d7d6e23e6a854ebb78504
Mfalme <calebrutto91@gmail.com>
2026-09-21 18:49:41 +0300

    Add expected credit loss calculation function

---
+++ src/ecl.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def expected_credit_loss(pd_, lgd, ead):
+    pd_ = pd.Series(pd_)
+    lgd = pd.Series(lgd)
+    ead = pd.Series(ead)
+    return (pd_ * lgd * ead).rename('ecl')
+
+if __name__ == '__main__':
+    out = expected_credit_loss([0.04, 0.10], [0.6, 0.5], [10000, 20000])
+    print(out.tolist())
