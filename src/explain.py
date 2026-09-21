5253194a50862b25b78b82ca21d2832171351cc7
Mfalme <calebrutto91@gmail.com>
2026-09-21 19:16:53 +0300

    Add top_drivers function to explain model predictions

---
+++ src/explain.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import joblib
+
+def top_drivers(model, x_row, k=5):
+    prep = model.named_steps['prep']
+    clf = model.named_steps['clf']
+    x_t = prep.transform(pd.DataFrame([x_row]))
+    names = prep.get_feature_names_out()
+    coefs = clf.coef_[0]
+    contrib = x_t.toarray()[0] * coefs
+    idx = np.argsort(np.abs(contrib))[::-1][:k]
+    return [(names[i], float(contrib[i])) for i in idx]
+
+if __name__ == '__main__':
+    from sklearn.datasets import fetch_openml
+    data = fetch_openml(name='credit-g', version=1, as_frame=True)
+    X = data.frame.drop(columns=['class'])
+    model = joblib.load('models/pd_baseline.joblib')
+    print(top_drivers(model, X.iloc[0].to_dict()))
