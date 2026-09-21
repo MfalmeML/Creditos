c81f7e245d71e8340b311038db4455fe59f07449
Mfalme <calebrutto91@gmail.com>
2026-09-21 18:49:14 +0300

    Add EAD estimation function and example usage

---
+++ src/ead.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+def estimate_ead(balance, ccf=0.75, limit=None):
+    balance = pd.Series(balance)
+    if limit is None:
+        return balance.rename('ead')
+    undrawn = (pd.Series(limit) - balance).clip(lower=0)
+    return (balance + ccf * undrawn).rename('ead')
+
+if __name__ == '__main__':
+    bal = [1000, 2000, 500]
+    lim = [5000, 5000, 1000]
+    print(estimate_ead(bal, limit=lim).tolist())
