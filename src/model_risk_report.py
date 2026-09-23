6c1496181e98e561340a6d4d0174046955bfa6b3
Mfalme <calebrutto91@gmail.com>
2026-09-23 19:14:56 +0300

    Refactor model risk report to improve data handling and clarity

--- src/model_risk_report.py
+++ src/model_risk_report.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import joblib
 from sklearn.datasets import fetch_openml
 from src.drift import data_drift
@@ -6,10 +7,12 @@ from src.prediction_drift import prediction_drift
 
 def model_risk_report(model_path='models/pd_baseline.joblib'):
     model = joblib.load(model_path)
-    data = fetch_openml(name='credit-g', version=1, as_frame=True)
-    X = data.frame.drop(columns=['class'])
-    half = len(X) // 2
-    ref_X, cur_X = X.iloc[:half], X.iloc[half:]
+    train_data = fetch_openml(name='credit-g', version=1, as_frame=True)
+    ref_X = train_data.frame.drop(columns=['class'])
+
+    cur = pd.read_csv('data/decisions_full.csv')
+    cur_X = cur[ref_X.columns]
+
     dd = data_drift(ref_X, cur_X)
     pp = prediction_drift(
         model.predict_proba(ref_X)[:, 1],
