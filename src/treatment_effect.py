78f2daa1638578e14c0db694914c82d43737164f
Mfalme <calebrutto91@gmail.com>
2026-09-23 18:15:33 +0300

    Add treatment_effect.py for calculating treatment effects using uplift model

---
+++ src/treatment_effect.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import joblib
+
+def treatment_effect(model, x_row):
+    X = pd.DataFrame([x_row])
+    p0 = model.m0.predict_proba(X)[:, 1][0]
+    p1 = model.m1.predict_proba(X)[:, 1][0]
+    return {
+        'pd_no_intervention': float(p0),
+        'pd_with_intervention': float(p1),
+        'treatment_effect': float(p1 - p0),
+    }
+
+if __name__ == '__main__':
+    model = joblib.load('models/uplift_tlearner.joblib')
+    print(treatment_effect(model, {'age': 30, 'credit_amount': 12000}))
+    print(treatment_effect(model, {'age': 55, 'credit_amount': 2000}))
